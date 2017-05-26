0525127551e16aef8329df20ce82a41c2b6b46dc
helton.andreazza <helton.andreazza@senior.com.br>
2017-05-26 13:59:30 -0300

    changes app components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { DemoComponent } from "app/demo/demo.component";
+import { DemoModule } from "app/demo/demo.module";
+import { DemoResolver } from "app/demo/demo.resolver";
+import { PermissionModule } from './permission/permission.module';
 
 @NgModule({
   declarations: [
@@ -11,8 +17,20 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    CoreModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    DemoModule,
+    PermissionModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: DemoComponent,
+        resolve: {
+          userPermissions: DemoResolver
+        }
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
